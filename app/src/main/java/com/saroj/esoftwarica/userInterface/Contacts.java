fc46845a1bcf5046c0fe8f912922035415500b63
sarojpathak <sarojpathak430@gmail.com>
2019-12-25 20:16:22 +0545

    fragment classes added

---
+++ app/src/main/java/com/saroj/esoftwarica/userInterface/Contacts.java
@@ -0,0 +1,71 @@
+package com.saroj.esoftwarica.userInterface;
+
+public class Contacts {
+    public Contacts(String name, String address, String gender, int age) {
+        Name = name;
+        Address = address;
+        Gender = gender;
+        Age = age;
+        this.imgProfileId = imgProfileId;
+        this.imgRemoveId = imgRemoveId;
+    }
+
+    private String Name;
+    private String Address;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getImgProfileId() {
+        return imgProfileId;
+    }
+
+    public void setImgProfileId(int imgProfileId) {
+        this.imgProfileId = imgProfileId;
+    }
+
+    public int getImgRemoveId() {
+        return imgRemoveId;
+    }
+
+    public void setImgRemoveId(int imgRemoveId) {
+        this.imgRemoveId = imgRemoveId;
+    }
+
+    private String Gender;
+    private int Age;
+    private int imgProfileId;
+    private  int imgRemoveId;
+
+
+}
+
---
+++ app/src/main/java/com/saroj/esoftwarica/userInterface/notification/NotificationsViewModel.java
@@ -0,0 +1,20 @@
+package com.saroj.esoftwarica.userInterface.notification;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Notification");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
